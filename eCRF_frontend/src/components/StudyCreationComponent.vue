7a256e7a3b775072bc343767080e1e03e64c0f6b
Venkatesh Hariharapura Shivashankar <venkateshhs.shivashankar@gmail.com>
2025-03-24 12:57:10 +0100

    Fixes #13

--- eCRF_frontend/src/components/StudyCreationComponent.vue
+++ eCRF_frontend/src/components/StudyCreationComponent.vue
@@ -211,6 +211,8 @@ export default {
     validateAndProceed() {
       this.showErrors = true;
       if (!this.customStudy.name || !this.customStudy.description) return;
+      localStorage.removeItem("studyDetails");
+      localStorage.removeItem("scratchForms");
       const studyDetails = {
         name: this.customStudy.name,
         description: this.customStudy.description,
